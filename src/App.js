7a83d021aaa71a22feb31cd91b35f4f62cd17985
Kurniaman Andreas Zega <nimanz720@gmail.com>
2022-08-08 23:07:09 +0700

    update Homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'assets/css/app.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from 'pages/HomePage';
- 
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={HomePage} /> 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
       </Router>
     </div>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
-import React from 'react'
 import Header from 'parts/Header';
+import Hero from 'parts/Hero';
+import React from 'react'
 
 export default function HomePage(props) {
   return (
     <>
-    <Header></Header>
+      <Header />
+      <Hero />
     </>
   );
 }
